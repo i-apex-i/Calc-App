20f62748588debde08b08aa776701ac158558365
i-apex-i <mmatthewdavis@yahoo.co.uk>
2017-03-17 15:28:10 +0000

    Add files via upload
    version 2

---
+++ 13044960_Project/JuniorArithmetics/JuniorArithmetics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JuniorArithmetics
+//
+//  Created by md13aba on 20/02/2017.
+//  Copyright © 2017 md13aba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
